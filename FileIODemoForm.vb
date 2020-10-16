fb9a1e9083fcf35c11cf01bc91338211167ac39d
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-10-16 08:58:18 -0600

    add dontbedumb sub

---
+++ FileIODemoForm.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+
+Public Class FileIODemoForm
+    Sub DontBeDumb() Handles Me.Load
+        FileOpen(1, "C:\Users\Jake Burrell\Documents\_FileDemoStuff\myfile.txt", OpenMode.Output) 'override
+        'FileOpen(1, ""C:\Users\Jake Burrell\Documents\_FileDemoStuff\myfile.txt"", OpenMode.Input)
+        FileOpen(1, ""C:\Users\Jake Burrell\Documents\_FileDemoStuff\myfile.txt"", OpenMode.Append) 'adds to the end of the flie
+        WriteLine(1, "here is data")
+        FileClose(1)
+    End Sub
+
+
+End Class
